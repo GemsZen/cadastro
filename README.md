0c6a327807295b826019c9fcd6505e5b809c0eff
GemsZen <GemsZen@users.noreply.github.com>
2023-04-26 00:17:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cadastro
+ Caixa de cadastro python 
---
+++ cadastro.py
@@ -0,0 +1,19 @@
+from PySimpleGUI import PySimpleGUI as sg 
+
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'),sg.Input(key='usuario',size=(20,1))],
+    [sg.Text('Senha'),sg.Input(key='senha',password_char='*',size=(20,1), )],
+    [sg.Checkbox('Salvar o login?')],
+    [sg.Button('Entrar')]
+]
+
+janela = sg.Window('Tela de Login',layout)
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'gems' and valores['senha'] == '123456':
+            print('Bem-vindo jovem programador!')
